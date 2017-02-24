98ebe8657bdeba5f40f71aaa5fd55c78f89e5b85
smedapati1 <medapati.soujanya1@gmail.com>
2017-02-24 15:04:30 -0600

    updated as per client request

--- adWidget.js
+++ adWidget.js
@@ -6,23 +6,28 @@ var x = window.adWidget_x ? window.adWidget_x:1;
 var y = window.adWidget_y ? window.adWidget_y:4;
 var d= window.d ? window.d: 'WIDGET_abd0';
 var ad_library= [{
-  img_url:'./featured2-cb0dc81d.jpg',
-  ad_text:'HI there 1',
+  img_url:'./sample.png',
+  ad_text:'Hi there',
   ad_hyperlink:'http://google.com'
 },
 {
-  img_url:'./featured2-cb0dc81d.jpg',
-  ad_text:'HI there',
+  img_url:'./sample.png',
+  ad_text:'Hi there',
   ad_hyperlink:'http://google.com'
 },
 {
-  img_url:'./featured2-cb0dc81d.jpg',
-  ad_text:'HI there',
+  img_url:'./sample.png',
+  ad_text:'Hi there',
   ad_hyperlink:'http://google.com'
 },
 {
-  img_url:'./featured2-cb0dc81d.jpg',
-  ad_text:'HI there',
+  img_url:'./sample.png',
+  ad_text:'Hi there',
+  ad_hyperlink:'http://google.com'
+},
+{
+  img_url:'./sample.png',
+  ad_text:'Hi there',
   ad_hyperlink:'http://google.com'
 }
 ];
@@ -30,13 +35,13 @@ var ad_library= [{
 createAdCanvas(x,y);
 
 function createAdCanvas(x,y) {
-  var element_code='<div style="padding-right:5px;">';
-  element_code += '<span style=" text-align: left;font-weight: bold;color:'+ document.getElementsByTagName('h1')[0].style.color+'">You Might Also Like</span>  <span class="ad_owner">Powered by MyAdCompany</span></div>';
+  var element_code='';
+  element_code += '<span class = "ad_caption" style=" text-align: left;font-weight: bold;color:'+ document.getElementsByTagName('h1')[0].style.color+'">You Might Also Like</span>';
   var row_html='',ad_incrementor =0;
 
   var ad_data='  <figure class="ad_container"> <img src="./featured2-cb0dc81d.jpg" alt="Smiley face" class="image_container"> <figcaption><p>Put your text here </p></figcaption> </figure>';
   for(var i=0;i<y;i++){
-    row_html=' <div style="clear:both;">';
+    row_html=' <div style="clear:both;width:100%;">';
     for(var j=0;j<x;j++){
      //code logic 1  : use this if you want to fill all x-y blocks with ads no matter how many or few ads you have
      // row_html += createAd(ad_incrementor);
@@ -57,6 +62,7 @@ function createAdCanvas(x,y) {
     row_html +=' </div>';
     element_code += row_html;
   }
+  element_code += '<span class="ad_owner">Powered by MyAdCompany</span>'
   var ls = document.createElement('link');
   ls.rel = "stylesheet";
   ls.href = 'inde.css';
@@ -71,8 +77,8 @@ function createAdCanvas(x,y) {
 
 function createAd(ad_index){
   var ad_details = ad_library[ad_index];
-  console.log(ad_details);
-   var ad_html='<figure class="ad_container"> <img src="' + ad_details.img_url +'" alt="Smiley face" class="image_container"> <figcaption><a href = "'+
-   ad_details.ad_hyperlink + '">'+ ad_details.ad_text +'</a></figcaption> </figure>';
+  image_width=100/x-2;
+  var ad_html='<figure class="ad_container" style="width:'+image_width+'%"> <img src="' + ad_details.img_url +'" alt="Smiley face" class="image_container"> <figcaption><a href = "'+
+   ad_details.ad_hyperlink + '"  style = "font-family:Arial;font-weight:bold ;color:'+document.getElementsByTagName('h1')[0].style.color+'">'+ ad_details.ad_text +'</a></figcaption> </figure>';
  return ad_html;
 }
